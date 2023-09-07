52f729b8fd64f089da76f819e8fd5dbce70fcd69
NLP <33448441+yangzhipeng1108@users.noreply.github.com>
2023-09-07 14:59:57 +0800

    Update dpo-7b.sh

--- dpo-7b.sh
+++ dpo-7b.sh
@@ -3,8 +3,8 @@ if [ ! -d ${output_model} ];then
     mkdir ${output_model}
 fi
 deepspeed --include localhost:0,1,2,3,4,5,6,7 --master_port 29000 dpo.py \
-    --model_name_or_path /root/llama/llama2-lora-fine-tuning/model/models--daryl149--llama-2-7b-chat-hf/snapshots/bbc9b373dacff93e600e4426f2b3d3dd264e90ed  \
-    --tokenizer_name /root/llama/llama2-lora-fine-tuning/merged_tokenizer_hf \
+    --model_name_or_path merged_model_output \
+    --tokenizer_name /root/llama/llama2-lora-fine-tuning/model/models--daryl149--llama-2-7b-chat-hf/snapshots/bbc9b373dacff93e600e4426f2b3d3dd264e90ed  \
     --train_files /root/llama1/rl_dpo/base/helpful_base_cn_train.jsonl \
     --validation_files  /root/llama1/rl_dpo/base/helpful_base_cn_test.jsonl \
     --per_device_train_batch_size 2 \
