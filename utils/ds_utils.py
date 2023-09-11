3d6c50e9bb2c3ac1b5bcc63fbbbe466a37409cba
NLP <33448441+yangzhipeng1108@users.noreply.github.com>
2023-09-11 14:56:52 +0800

    Create ds_utils.py

---
+++ utils/ds_utils.py
@@ -0,0 +1,118 @@
+# Copyright (c) Microsoft Corporation.
+# SPDX-License-Identifier: Apache-2.0
+
+# DeepSpeed Team
+GLOBAL_BATCH_SIZE = 32
+MICRO_BATCH_SIZE = 4
+
+
+def get_train_ds_config(offload,
+                        stage=2,
+                        enable_hybrid_engine=False,
+                        inference_tp_size=1,
+                        release_inference_cache=False,
+                        pin_parameters=True,
+                        tp_gather_partition_size=8,
+                        max_out_tokens=512):
+
+    device = "cpu" if offload else "none"
+    zero_opt_dict = {
+        "stage": stage,
+        "offload_param": {
+            "device": device
+        },
+        "offload_optimizer": {
+            "device": device
+        },
+        "overlap_comm": True,
+        "contiguous_gradients": True,
+        "reduce_scatter": True,
+        "stage3_param_persistence_threshold": 1e4,
+        "stage3_max_live_parameters": 1e4,
+        "stage3_prefetch_bucket_size": 1e4,
+        "sub_group_size": 1e4,
+        "stage3_gather_16bit_weights_on_model_save":False,
+        "memory_efficient_linear": False
+    }
+
+    # zero_opt_dict = {
+    #     "stage": 1,
+    #     "reduce_bucket_size": 5e8
+    # }
+    return {
+        "train_batch_size": GLOBAL_BATCH_SIZE,
+        "train_micro_batch_size_per_gpu": MICRO_BATCH_SIZE,
+        "steps_per_print": 10,
+        "zero_optimization": zero_opt_dict,
+        "bf16": {
+            "enabled": True,
+            "loss_scale_window": 100
+        },
+        "optimizer": {
+            "type": "Adam",
+            "params": {
+                "lr": 0.001,
+                "betas": [
+                    0.8,
+                    0.999
+                ],
+                "eps": 1e-8,
+                "weight_decay": 3e-7
+            }
+        },
+        "scheduler": {
+            "type": "WarmupLR",
+            "params": {
+                "warmup_min_lr": 0,
+                "warmup_max_lr": 0.001,
+                "warmup_num_steps": 1000
+            }
+        },
+        "gradient_clipping": 1.0,
+        "prescale_gradients": False,
+        "wall_clock_breakdown": False,
+        "hybrid_engine": {
+            "enabled": enable_hybrid_engine,
+            "max_out_tokens": max_out_tokens,
+            "inference_tp_size": inference_tp_size,
+            "release_inference_cache": release_inference_cache,
+            "pin_parameters": pin_parameters,
+            "tp_gather_partition_size": tp_gather_partition_size,
+        }
+    }
+
+
+def get_eval_ds_config(offload, stage=0):
+    device = "cpu" if offload else "none"
+    zero_opt_dict = {
+        "stage": stage,
+        "stage3_param_persistence_threshold": 1e4,
+        "offload_param": {
+            "device": device
+        },
+        "memory_efficient_linear": False
+    }
+    return {
+        "train_batch_size": GLOBAL_BATCH_SIZE,
+        "train_micro_batch_size_per_gpu": MICRO_BATCH_SIZE,
+        "steps_per_print": 10,
+        "zero_optimization": zero_opt_dict,
+        "fp16": {
+            "enabled": True
+        },
+        "optimizer": {
+            "type": "Adam",
+            "params": {
+                "lr": 0.001,
+                "betas": [
+                    0.8,
+                    0.999
+                ],
+                "eps": 1e-8,
+                "weight_decay": 3e-7
+            }
+        },
+        "gradient_clipping": 1.0,
+        "prescale_gradients": False,
+        "wall_clock_breakdown": False
+    }
